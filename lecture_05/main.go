8afdfc191713b3837fd6ee912d5229c5c53519ac
“zobaer_zehad” <z.abdullah202@gmail.com>
2021-01-31 14:24:25 +0600

    setupdone

---
+++ lecture_05/main.go
@@ -0,0 +1,10 @@
+package main
+
+import"fmt"
+
+func main(){
+
+
+ fmt.Println("Hello bangnladesh")
+ fmt.Println("I love my country")
+}
---
+++ lecture_08/variable.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//var name string = "ZEHAD"
+//var name2 , city string = "ZEHAD" , "KISHOREGANJ"
+//fmt.Println(name , city ,name2)
+
+var c rune = 'A'
+var age int = 10
+var result float32 =50.95
+
+fmt.Println (c, age, result)
+
+}
