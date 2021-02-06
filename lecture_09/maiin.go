70bca62c105a8073d1cbc27da8e5517ef5d16c9a
“zobaer_zehad” <z.abdullah202@gmail.com>
2021-02-06 14:46:10 +0600

    primitive

---
+++ lecture_09/maiin.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+//var name string
+//var age int
+//var result float32
+//var chr rune 
+var isFound bool
+
+  fmt.Println(isFound)
+  fmt.Println(true && true)
+  fmt.Println(true && false)
+  fmt.Println(true || true)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+
+}
