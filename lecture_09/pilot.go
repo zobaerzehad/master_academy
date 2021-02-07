3e0f8ea3e662c49f4a119513e164de1cd04e5689
“zobaer_zehad” <z.abdullah202@gmail.com>
2021-02-07 16:46:14 +0600

    primitive

---
+++ lecture_09/pilot.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//var name string
+//var age int
+//var result float32
+//var isFound bool
+
+//fmt.Println(name)
+//fmt.Println(age)
+//fmt.Println(result)
+var chr rune
+chr = 'B'
+fmt.Println(chr)
+fmt.Printf("%c" , chr)
+
+  //fmt.Println(isFound)
+  //fmt.Println(true && true)
+  //mt.Println(true && false)
+  //fmt.Println(true || true)
+  //fmt.Println(true || false)
+  //fmt.Println(!true)
+
+}
