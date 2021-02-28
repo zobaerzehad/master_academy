688a5a9e7f7606af5554df1582172fb0e3dc55a5
“zobaer_zehad” <z.abdullah202@gmail.com>
2021-02-28 21:53:08 +0600

    Network

---
+++ lecture_16/http/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main()  {
+
+	nl, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		fmt.Println(err.Error)
+		os.Exit(1)		
+	}	
+	conn, err := nl.Accept()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	bs := make([]byte, 1024)
+	n, e := conn.Read(bs)
+	if e != nil {
+		fmt.Println(e.Error())
+	}
+	fmt.Println(n)
+	reqstr := string(bs)
+	fmt.Println(reqstr)
+
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><strong>Hello world</strong></body></html>`
+	
+	resp := "HTTP/1.1 200 OK\r\n" +
+		"Content-Length: %d\r\n" +
+		"Content-Type: text/html\r\n"+
+		 "\r\n%s"
+
+	msg := fmt.Sprintf(resp, len(body), body)
+	fmt.Println(msg)
+	conn.Write([]byte(msg))
+
+}
